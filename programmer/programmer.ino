dfcdc4ab41e88f4099e80393a691d1271b858681
restart82 <shapovalov_petya@mail.ru>
2023-04-05 21:01:06 +0300

    Update programmer.ino
    Первая версия программы

--- programmer/programmer.ino
+++ programmer/programmer.ino
@@ -1,32 +1,51 @@
-const int BUFFER_SIZE = 50;
-char buf[BUFFER_SIZE];
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
+    //назначим режим работы пинов:
+    pinMode(22, OUTPUT);
+    pinMode(23, OUTPUT);
+    pinMode(24, OUTPUT);
+    pinMode(25, OUTPUT);
+    pinMode(26, INPUT);
+    pinMode(27, INPUT);
+    pinMode(28, INPUT);
+    pinMode(29, INPUT);
+    // можно еще задать режим INPUT_PULLUP - вход с подтяжкой
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   if (Serial.available() > 0){
-    //int in_data_size = Serial.readBytes(buf, BUFFER_SIZE);
-    uint8_t in_data = Serial.read();
-    //int in_data = Serial.parseInt();
-    //Serial.println(in_data);
-    //Serial.print(in_data);
-    //Serial.println(buf);
 
-    int pb0 = bitRead(in_data, 0);
-    int pb1 = bitRead(in_data, 1);
-    int pb2 = bitRead(in_data, 2);
-    int power = bitRead(in_data, 3);
-    Serial.println();
-    Serial.print(pb0);
-    Serial.print(pb1);
-    Serial.print(pb2);
-    Serial.print(power);
+    uint8_t in_data = Serial.read(); //читаем байт
 
-  }
+    bool pb0 = bitRead(in_data, 0);
+    bool pb1 = bitRead(in_data, 1);
+    bool pb2 = bitRead(in_data, 2);
+    bool power = bitRead(in_data, 3);
+
+    //выводим биты на соответствующие пины
+    digitalWrite(22, pb0);
+    digitalWrite(23, pb1);
+    digitalWrite(24, pb2);
+    digitalWrite(25, power);
+
+    uint8_t out_data;
+
+    //заполняем выходной байт с соответствующих пинов
+    bitWrite(out_data, 0, digitalRead(26));
+    bitWrite(out_data, 1, digitalRead(27));
+    bitWrite(out_data, 2, digitalRead(28));
+    bitWrite(out_data, 3, digitalRead(29));
+    
+    Serial.print(out_data);
+    //Serial.print(pb0);
+    //Serial.print(pb1);
+    //Serial.print(pb2);
+    //Serial.print(power);
+
+      }
 
 }
